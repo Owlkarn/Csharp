e97661743b5d31ee1e2405178b34457c4697551b
Vedeneev Ilya <tegrozayadc@yandex.ru>
2022-08-05 21:26:42 +0500

    task 2 done

--- Домашнее_задание/1_семинар/Задача_2/Program.cs
+++ Домашнее_задание/1_семинар/Задача_2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("Первое число больше второго, т.к. " + number1 + " больше чем " + number2);
+}
+else
+{
+    Console.WriteLine("Второе число больше первого, т.к. " + number2 + " больше чем " + number1);
+}
