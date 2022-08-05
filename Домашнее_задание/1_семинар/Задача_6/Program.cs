b299598efe5546cea5b1022fd371bfde0182b9aa
Vedeneev Ilya <tegrozayadc@yandex.ru>
2022-08-05 21:44:25 +0500

    task 6 done

--- Домашнее_задание/1_семинар/Задача_6/Program.cs
+++ Домашнее_задание/1_семинар/Задача_6/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()),
+    ostatok = number % 2;
+
+if (ostatok == 0)
+{
+    Console.Write("Число четное");
+}
+else
+{
+    Console.Write("Число нечетное");
+}
