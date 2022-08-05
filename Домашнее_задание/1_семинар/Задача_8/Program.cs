a67b976d8188ae5d588175cdf6c7065000167ae1
Vedeneev Ilya <tegrozayadc@yandex.ru>
2022-08-05 21:55:59 +0500

    task 8 done

--- Домашнее_задание/1_семинар/Задача_8/Program.cs
+++ Домашнее_задание/1_семинар/Задача_8/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine()),
+    count = 1;
+
+if (N > 0)
+{
+    while (count <= N)
+    {
+        if (count % 2 == 0 && count != 0)
+        {
+            Console.Write(count + ", ");
+        }
+    count++;
+    }
+}
+else
+{
+    while (count >= N)
+    {
+        if (count % 2 == 0 && count != 0)
+        {
+            Console.Write(count + ", ");
+        }
+    count--;
+    }
+}
