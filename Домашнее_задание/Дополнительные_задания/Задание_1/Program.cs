9e3d49777540c951d347ae7e7547e262112e579e
Vedeneev Ilya <tegrozayadc@yandex.ru>
2022-08-06 01:18:51 +0500

    hard task 1 done

--- Домашнее_задание/Дополнительные_задания/Задание_1/Program.cs
+++ Домашнее_задание/Дополнительные_задания/Задание_1/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿Console.Write("Введите пароль: ");
+string tryPassword = Console.ReadLine(),
+       realPassword = "geekbrains",
+       secretMessage = "Образовательная платформа";
+int count = 0;
+
+while (count < 4)
+{
+    if (tryPassword.ToLower() == realPassword)
+    {
+        Console.Write("Образовательная платформа");
+        count = 5;
+    }
+    else if (count != 2)
+    {
+        Console.Write("Пароль неверный, введите пароль: ");
+        tryPassword = Console.ReadLine();
+        count++;
+    }
+    else
+    {
+        Console.WriteLine("Пароль неверный. Попытки закончились");
+        count = 5;
+    }
+}
