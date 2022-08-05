1663119fe5b20cad2fba928725bdaa24d55f776b
Vedeneev Ilya <tegrozayadc@yandex.ru>
2022-08-06 01:28:07 +0500

    hard task 2 done

--- Домашнее_задание/Дополнительные_задания/Задание_2/Program.cs
+++ Домашнее_задание/Дополнительные_задания/Задание_2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string writeEnd = " ",
+       programmEnd = "exit";
+
+while (programmEnd != writeEnd)
+{
+    Console.Write("Может закончим? ");
+    writeEnd = Console.ReadLine();
+}
