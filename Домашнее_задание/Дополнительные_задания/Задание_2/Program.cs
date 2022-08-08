3d684871d1096ac1bd7a44f424529fc1c8c8d0dc
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-08 16:12:12 +0500

    hard task 2 complete

--- Домашнее_задание/Дополнительные_задания/Задание_2/Program.cs
+++ Домашнее_задание/Дополнительные_задания/Задание_2/Program.cs
@@ -1,8 +1,16 @@
-﻿string writeEnd = " ",
-       programmEnd = "exit";
+﻿ConsoleKeyInfo cki = new ConsoleKeyInfo();
+string exit = "";
 
-while (programmEnd != writeEnd)
-{
-    Console.Write("Может закончим? ");
-    writeEnd = Console.ReadLine();
-}
+            while (true)
+            {
+                Console.Write("1 ");
+                if (Console.KeyAvailable == true)
+                {
+                    cki = Console.ReadKey(true);
+                    exit += cki.KeyChar.ToString();
+                }
+                if (exit.Contains("exit")) break;                  
+            }
+                            
+            Console.Write("Цикл завершился");
+            Console.ReadKey();
