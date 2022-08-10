b3015d3da298d0d36286207e8f7899762d2ab9c9
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-10 14:08:49 +0500

    task 15 done

--- Домашнее_задание/2_семинар/Задача_15/Program.cs
+++ Домашнее_задание/2_семинар/Задача_15/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadInt("Введите номер дня недели: ");
+
+while(number < 1 || number > 7)
+{
+    number = ReadInt("Введено некорректное значение. Введите номер дня недели: ");
+}
+if (number > 5 && number < 8)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Рабочий день");
+}
