d1c4c3f9805b71985ada079da49ebf48ae90881b
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-10 11:27:31 +0500

    Homework 2 start

---
+++ Задание_20/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
---
+++ Задание_21/Program.cs
@@ -0,0 +1,21 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number1 = ReadInt("Введите первое число: ");
+int number2 = ReadInt("Введите второе число: ");
+
+if (number1 == number2 && number1 != 1)
+{
+    Console.WriteLine("Введены одинаковые числа");
+}
+if (number1 / number2 == number2 || number2 / number1 == number1)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
