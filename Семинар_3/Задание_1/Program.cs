62376c99fdc4d911ac51afe5eb36fcfe12533c9c
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-12 15:20:22 +0500

    Seminar 3 start

---
+++ Семинар_3/Задание_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x = ReadInt("Введите X: ");
+int y = ReadInt("Введите Y: ");
+
+if (x == 0 || y == 0) 
+{
+Console.WriteLine("Номер четверти определить не удалось");
+return;
+}
