9c2f2e107f3fca4b14163fd2e65dc687bfa197e3
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-15 13:32:53 +0500

    Homework 3 done

---
+++ Домашнее_задание/3_семинар/Задание_19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите 5-тизначное число: ");
+string vhod = Console.ReadLine();
+string rev = "";
+
+while (vhod.Length != 5 || Convert.ToInt32(vhod) < 0)
+{
+    Console.WriteLine("Введено неверное число. Введите 5-тизначное число: ");
+    vhod = Console.ReadLine();
+}
+
+for (int i = vhod.Length - 1; i >= 0; i--)
+{
+    rev += vhod[i]; 
+
+}
+if (rev == vhod)
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
+
---
+++ Домашнее_задание/3_семинар/Задание_21/Program.cs
@@ -0,0 +1,15 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ax = ReadInt("Введите координату X первой точки: ");
+int ay = ReadInt("Введите координату Y первой точки: ");
+int az = ReadInt("Введите координату Z первой точки: ");
+int bx = ReadInt("Введите координату X второй точки: ");
+int by = ReadInt("Введите координату Y второй точки: ");
+int bz = ReadInt("Введите координату Z второй точки: ");
+double rasst = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+
+Console.WriteLine($"Длина отрезка: {rasst}");
