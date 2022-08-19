a4e4d860890ba82736ad5971f9fc8290d3493bcc
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-19 15:25:14 +0500

    Seminar 5

---
+++ Семинар_5/Задание_1/Program.cs
@@ -0,0 +1,33 @@
+﻿int size = 12;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int sumNegativ = 0;
+int sumPositiv = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+        sumPositiv += numbers[i];
+        else
+        sumNegativ += numbers[i];
+    }
+
+Console.WriteLine($"Сумма положительных элементов = {sumPositiv} \nСумма отрицательных элементов = {sumNegativ}");
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ Семинар_5/Задание_2/Program.cs
@@ -0,0 +1,11 @@
+﻿void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+}
+
+Console.
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
