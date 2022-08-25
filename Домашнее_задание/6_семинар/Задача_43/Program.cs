db1886ed2ef86c984bebfa9f3b02f88b46ab85ab
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-25 15:13:40 +0500

    Homework 6 end

---
+++ Домашнее_задание/6_семинар/Задача_43/Program.cs
@@ -0,0 +1,28 @@
+﻿double ReadDouble(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+    
+}
+Console.WriteLine("Даны две прямые, заданные уравнениями: y = k1 * x + b1 и y = k2*x + b2");
+
+double k1 = ReadDouble("Введите переменную k1: ");
+double b1 = ReadDouble("Введите переменную b1: ");
+double k2 = ReadDouble("Введите переменную k2: ");
+double b2 = ReadDouble("Введите переменную b2: ");
+double x = 0;
+double y = 0;
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    Console.WriteLine($"Координаты точки пересечения двух прямых: ({x};{y})");
+}
+
+
+
