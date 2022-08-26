85f7b3c189c87eaf2e7eb6ea02d90a37633e6c33
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-26 13:38:59 +0500

    Practice 7 start..

--- Семинар_7/Задание_1/Program.cs
+++ Семинар_7/Задание_1/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-5, 9);
+        }
+    }
+}
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+
---
+++ Семинар_7/Задание_2/Program.cs
@@ -0,0 +1,31 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int n = ReadInt("Введите количество строк: ");
+int m = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[n,m];
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        numbers[i,j] = i + j;
+    }
+}
+PrintArray2D(numbers);
