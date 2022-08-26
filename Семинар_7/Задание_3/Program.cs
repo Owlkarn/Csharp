9fefb0b87cad64f2d293cf302d1b8a4ae005a9f7
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-26 13:46:02 +0500

    Practice 7 start...

--- Семинар_7/Задание_3/Program.cs
+++ Семинар_7/Задание_3/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(2, 10);
+        }
+    }
+}
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+for (int i = 1; i < numbers.GetLength(0); i = i + 2)
+{
+    for (int j = 1; j < numbers.GetLength(1); j = j + 2)
+    {
+        numbers[i, j] *= numbers[i, j];
+    }
+}
+PrintArray2D(numbers);
