001e05c619e46d48f1fd341d8965e41112fab3cb
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-26 15:44:07 +0500

    Homework 7 in progress

--- Домашнее_задание/7_семинар/Задача_47/Program.cs
+++ Домашнее_задание/7_семинар/Задача_47/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,6:F2}", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-9, 10) + new Random().NextDouble();
+        }
+    }
+}
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+double[,] numbers = new double[rows, columns];
+FillArray2D(numbers);
+PrintArray2D(numbers);
--- Домашнее_задание/7_семинар/Задача_50/Program.cs
+++ Домашнее_задание/7_семинар/Задача_50/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(2, 10);
+        }
+    }
+}
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int findElement = ReadInt("Какой элемент найти? :");
+int[,] numbers = new int[rows, columns];
+int findRow = findElement / 10;
+int findColumn = findElement % 10;
+
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+
