c6e1ee3034b4995b7fefa06b279608ad6d536abd
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-09-01 15:28:41 +0500

    Hardtask 7 done

---
+++ Домашнее_задание/Дополнительные_задания/Задание_7/Program.cs
@@ -0,0 +1,59 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void Shuffle(int[,] array)
+{
+    int temp = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int iTemp = new Random().Next(0, array.GetLength(0));
+            int jTemp = new Random().Next(0, array.GetLength(1));
+            temp = array[i, j];
+            array[i, j] = array[iTemp, jTemp];
+            array[iTemp, jTemp] = temp;
+        }
+    }
+}
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+
+FillArray2D(numbers);
+PrintArray2D(numbers);
+Shuffle(numbers);
+Console.WriteLine();
+PrintArray2D(numbers);
+
+
+
