f53b0ca0b4cb80eabafcbfba3a81560dde299371
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-09-02 15:38:24 +0500

    Practice 9 in work

---
+++ Семинар_9/Задание_1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(NaturalNumber(number));
+
+int NaturalNumber(int n)
+{
+    if (n == 1) return 1;
+    else
+    {
+        Console.Write(NaturalNumber(n - 1) + " ");
+    }
+
+    return n;
+}
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// NaturalNumber(number);
+
+// void NaturalNumber(int n)
+// {
+//     if (n == 1)
+//         Console.Write(n + " ");
+//     else
+//     {
+//         NaturalNumber(n - 1);
+//         Console.Write(n + " ");
+//     }
+// }
---
+++ Семинар_9/Задание_2/Program.cs
@@ -0,0 +1,29 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void NaturalNumber(int m, int n)
+{
+    if (m < n)
+    {
+        Console.Write($"{m}, ");
+        NaturalNumber(m + 1, n);
+    }
+    if (m > n)
+    {
+        Console.Write($"{m}, ");
+        NaturalNumber(m - 1, n);
+    }
+    if (m == n)
+    {
+        Console.Write($"{m} ");
+    }
+}
+
+int numberN = ReadInt("Введите число N: ");
+int numberM = ReadInt("Введите число M: ");
+
+NaturalNumber(numberM, numberN);
+
