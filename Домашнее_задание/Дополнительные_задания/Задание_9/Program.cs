52d1cac99be02b258e91670f253d0cf0ee99a115
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-09-08 08:42:34 +0500

    Hardtask 9 start 2

--- Домашнее_задание/Дополнительные_задания/Задание_9/Program.cs
+++ Домашнее_задание/Дополнительные_задания/Задание_9/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿int[] ParseStringToArray(string input)
+{
+    int countNumbers = GetCountNumbersInString(input);
+    int[] numbers = new int[countNumbers];
+    int indexNumber = 0;
+    string substring = string.Empty;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] != ' ')
+        {
+            substring += input[i];
+        }
+        else
+        {
+            numbers[indexNumber] = Convert.ToInt32(substring);
+            indexNumber++;
+            substring = string.Empty;
+        }
+    }
+    if (input[input.Length - 1] != ' ')
+        numbers[indexNumber] = Convert.ToInt32(substring);
+    return numbers;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("Текйщий массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
