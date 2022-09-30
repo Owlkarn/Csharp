8794ff7ad1921ea5a8e9f9a606f9a5566faa2b30
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-09-30 11:58:37 +0500

    Hardtask 10.1 fix

--- Домашнее_задание/Дополнительные_задания/Задание_10/Program.cs
+++ Домашнее_задание/Дополнительные_задания/Задание_10/Program.cs
@@ -20,58 +20,63 @@ void PrintArray2D(string[,] array)
     Console.WriteLine();
 }
 
-Console.CursorVisible = false;
-ConsoleKeyInfo k;
-
-int playerX = 7;
-int playerY = 0;
-char playerChar = '@';
-int exitX = 5;
-int exitY = 6;
-
-do
+void Labyrinth(int playerX, int playerY, string[,] field, int exitX, int exitY)
 {
-    Console.Clear();
-
-    string[,] field = new string[7, 15];
-
-
-    for (int i = 0; i < field.GetLength(0); i++)
-    {
-        for (int j = 0; j < field.GetLength(1); j++)
-        {
-            if (i == 0 || i == field.GetLength(0) - 1 || j == 0 || j == field.GetLength(1) - 1)
-                field[i, j] = "*";
-            else
-                field[i, j] = " ";
-        }
-    }
-    field[0, 7] = field[exitY, exitX] = " ";
-    field[1, 2] = field[1, 6] = field[1, 8] = field[2, 2] = field[2, 4] = field[2, 8] = field[2, 9] = field[2, 10] = field[2, 11] = field[2, 12] = field[3, 2] = field[3, 4] = field[3, 5] = field[3, 6] = field[3, 10] = field[3, 12] = field[4, 2] = field[4, 4] = field[4, 6] = field[4, 7] = field[4, 8] = field[5, 7] = field[5, 10] = field[5, 12] = field[0, 0];
-
-    PrintArray2D(field);
-
-    Console.SetCursorPosition(playerX, playerY);
-    Console.Write(playerChar);
-
-    k = Console.ReadKey(true);
-
-    if (k.Key == ConsoleKey.UpArrow && field[playerY - 1, playerX] != "*")
-        playerY--;
-    else if (k.Key == ConsoleKey.DownArrow && field[playerY + 1, playerX] != "*")
-        playerY++;
-    else if (k.Key == ConsoleKey.LeftArrow && field[playerY, playerX - 1] != "*")
-        playerX--;
-    else if (k.Key == ConsoleKey.RightArrow && field[playerY, playerX + 1] != "*")
-        playerX++;
-
-    if (playerX == exitX && playerY == exitY)
+    ConsoleKeyInfo k;
+    char playerChar = '@';
+    do
     {
         Console.Clear();
-        Console.WriteLine("YOU WIN!");
-        break;
-    }
+        PrintArray2D(field);
+        Console.SetCursorPosition(playerX, playerY);
+        Console.Write(playerChar);
 
-} while (k.Key != ConsoleKey.Escape);
+
+        k = Console.ReadKey(true);
+
+        if (k.Key == ConsoleKey.UpArrow && field[playerY - 1, playerX] != "*")
+            playerY--;
+        else if (k.Key == ConsoleKey.DownArrow && field[playerY + 1, playerX] != "*")
+            playerY++;
+        else if (k.Key == ConsoleKey.LeftArrow && field[playerY, playerX - 1] != "*")
+            playerX--;
+        else if (k.Key == ConsoleKey.RightArrow && field[playerY, playerX + 1] != "*")
+            playerX++;
+
+        if (playerX == exitX && playerY == exitY)
+        {
+            Console.Clear();
+            Console.WriteLine("YOU WIN!");
+            break;
+        }
+
+    } while (k.Key != ConsoleKey.Escape);
+}
+
+Console.CursorVisible = false;
+
+int startX = 7;
+int startY = 0;
+int stopX = 5;
+int stopY = 6;
+
+string[,] field1 = new string[7, 15];
+
+
+for (int i = 0; i < field1.GetLength(0); i++)
+{
+    for (int j = 0; j < field1.GetLength(1); j++)
+    {
+        if (i == 0 || i == field1.GetLength(0) - 1 || j == 0 || j == field1.GetLength(1) - 1)
+            field1[i, j] = "*";
+        else
+            field1[i, j] = " ";
+    }
+}
+field1[0, 7] = field1[stopY, stopX] = " ";
+field1[1, 2] = field1[1, 6] = field1[1, 8] = field1[2, 2] = field1[2, 4] = field1[2, 8] = field1[2, 9] = field1[2, 10] = field1[2, 11] = field1[2, 12] = field1[3, 2] = field1[3, 4] = field1[3, 5] = field1[3, 6] = field1[3, 10] = field1[3, 12] = field1[4, 2] = field1[4, 4] = field1[4, 6] = field1[4, 7] = field1[4, 8] = field1[5, 7] = field1[5, 10] = field1[5, 12] = field1[0, 0];
+
+PrintArray2D(field1);
+Labyrinth(startX, startY, field1, stopX, stopY);
 
 Console.CursorVisible = true;
--- Лекция_3/Задание_1/Program.cs
+++ Лекция_3/Задание_1/Program.cs
@@ -33,59 +33,18 @@
 // SelectionSort(arr);
 // PrintArray(arr);
 
-
-void PrintArray(string[] array)
+int ReadInt(string message)
 {
-    Console.Write("Массив: ");
-    for (int i = 0; i < array.Length; i++)
+    Console.Write(message);
+    string number = Console.ReadLine();
+    while (number == String.Empty)
     {
-        Console.Write(array[i] + " ");
+        Console.Write(message);
+        number = Console.ReadLine();
     }
-    Console.WriteLine();
+    return Convert.ToInt32(number);
 }
 
-string[] AddStringToArray(string[] array)
-{
-    Console.WriteLine("Введите элемент массива (чтобы закончить ввод элементов введите exit): ");
-    string str = Console.ReadLine();
-    int count = 0;
+int a = ReadInt("qwer");
 
-    while (str.ToLower() != "exit")
-    {
-        if (str != String.Empty)
-        {
-            count++;
-            string[] tempArray = new string[count];
-            for (int i = 0; i < count - 1; i++)
-                tempArray[i] = array[i];
-            tempArray[count - 1] = str;
-            array = tempArray;
-        }
-        Console.WriteLine("Введите элемент массива (чтобы закончить ввод элементов введите exit): ");
-        str = Console.ReadLine();
-    }
-    Console.WriteLine();
-    return array;
-}
-
-string[] lines = new string[0];
-lines = AddStringToArray(lines);
-PrintArray(lines);
-
-Console.Write("Введите длинну элемента массива, равной или меньше которой будет производиться поиск: ");
-int lgth = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-string[] filterLines = new string[0];
-for (int i = 0; i < lines.Length; i++)
-{
-    if (lines[i].Length <= lgth)
-    {
-        count++;
-        string[] tempFilterLines = new string[count];
-        for (int j = 0; j < count - 1; j++)
-            tempFilterLines[j] = filterLines[j];
-        tempFilterLines[count - 1] = lines[i];
-        filterLines = tempFilterLines;
-    }
-}
-PrintArray(filterLines);
+Console.WriteLine(a);
